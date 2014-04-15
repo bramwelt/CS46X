e32b1dde82566e06867f0eae6d061a41d1be5142
Trevor Bramwell <trevor@bramwell.net>
2014-04-15 15:07:43 -0700

    Update genbia, and include bidbundle from jwcrypto.

--- generate-assertion/genbia.js
+++ generate-assertion/genbia.js
@@ -1,64 +1,13 @@
 /*
- * Generate a Backed Identity Assertion, and create Certificates
+ * Generate a Backed Identity Assertion
  */
+var jwcrypto = require('/lib/jwcrypto');
+require('/lib/algs/ds');
 
-var jwcrypto = require("jwcrypto");
-require("jwcrypto/lib/algs/ds");
-require("jwcrypto/lib/algs/rs");
+AUDIENCE = "https://privlyalpha.org:443"
+EXPIRATION_LIMIT = (60 * 60 * 24 * 30); // 1 Month
 
-/*
-if (process.argv[2])
-  jwcrypto.setDataFormatVersion(process.argv[2]);
-
-var root_keypair;
-var user_keypair;
-var identity_cert;
-
-var EMAIL = "user@exampleidp.com";
-var ISSUED_AT = new Date();
-var EXPIRES_AT = new Date(new Date().valueOf() + 300);
-var ISSUER = "exampleidp.com";
-
-var test = function() {
-    jwcrypto.generateKeypair({algorithm: "RSA", keysize: 256}, function(err, keypair) {
-        console.log("ROOT RSA KEY");
-        console.log(keypair.publicKey.serialize());
-
-        console.log("ROOT RSA SECRET KEY");        
-        console.log(keypair.secretKey.serialize());
-        root_keypair = keypair;
-
-        // Create user keypair
-        jwcrypto.generateKeypair({algorithm: "DSA", keysize: 128}, function(err, keypair) {
-            console.log("USER DSA KEY");        
-            console.log(keypair.publicKey.serialize());
-
-            console.log("USER DSA SECRET KEY");        
-            console.log(keypair.secretKey.serialize());
-
-            user_keypair = keypair;
-
-            // Create cert
-            jwcrypto.cert.sign(
-                {publicKey: user_keypair.publicKey, principal: {email: EMAIL}},
-                {issuer: ISSUER, issuedAt: ISSUED_AT, expiresAt: EXPIRES_AT},
-                {}, root_keypair.secretKey, function(err, cert) {
-                    console.log("CERT");
-                    console.log(JSON.stringify({
-                      issued_at: ISSUED_AT.valueOf(),
-                      expires_at: EXPIRES_AT.valueOf(),
-                      issuer: ISSUER,
-                      email: EMAIL
-                    }));
-                    console.log(cert);
-                    identity_cert = cert;
-    
-                    generateBackedIdentityAssertion(identity_cert, user_keypair);
-            });
-        });
-    });
-};
-*/
+jwcrypto.addEntropy("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
 /*
  * Takes a User Certificate (identity cert), a secretkey, and a
@@ -68,18 +17,13 @@ var test = function() {
  */
 var generateBackedIdentityAssertion = function(cert, secretkey, cb) {
     var ISSUED_AT = new Date();
-    var EXPIRES_AT = new Date(new Date().valueOf() + 2590200); // 1 month
+    var EXPIRES_AT = new Date(new Date().valueOf() + EXPIRATION_LIMIT);
     jwcrypto.assertion.sign({},
       {expiresAt: EXPIRES_AT, issuedAt: ISSUED_AT,
-       audience: "https://privlyalpha.org"},
-       secretKey,
+       audience: AUDIENCE},
+       secretkey,
       function(err, assertion) {
         cb(err, jwcrypto.cert.bundle(cert, assertion));
       }
     );
 };
-
-//test();
-
-exports.sign = jwcrypto.sign
-exports.generateBackedIdentityAssertion = generateBackedIdentityAssertion
